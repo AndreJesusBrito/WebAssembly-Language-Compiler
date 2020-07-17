89f276ea2b53d92d87d838cd6ead2b6e990b1223
AndreJesusBrito <andrejesusdebrito@gmail.com>
2020-07-17 17:05:35 +0100

    changed ActionObj type definition

--- src/SyntaxAnalysis/types.ts
+++ src/SyntaxAnalysis/types.ts
@@ -1,11 +1,17 @@
 import { SyntaxRule } from "./SyntaxRule.ts";
 import { TerminalGroup } from "./TerminalGroup.ts";
+import { BaseNode } from "../TreeNodes/BaseNode.ts";
+import { Token } from "../LexicalAnalysis/Token.ts";
 
 export type SyntaxSymbol = string | SyntaxRule | TerminalGroup;
 
 export type ActionObj = {
-  index: number,
-  func: () => any,
+  index: (currentIndex: number) => number,
+  func: (
+    grammarStack: SyntaxSymbol[],
+    operatorStack: Token[],
+    nodeStack: BaseNode[],
+  ) => void,
 };
 
 export type RuleDerivation = {
