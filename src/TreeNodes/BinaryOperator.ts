ba03c3c89cc9ea637cf5e1f157447a2f0c808040
AndreJesusBrito <andrejesusdebrito@gmail.com>
2020-07-20 11:51:31 +0100

    added getters to operation node classes

--- src/TreeNodes/BinaryOperator.ts
+++ src/TreeNodes/BinaryOperator.ts
@@ -1,17 +1,27 @@
 import { ExpressionNode } from "./ExpressionNode.ts"
 
 export abstract class BinaryOperator extends ExpressionNode {
-  private operand1: ExpressionNode;
-  private operand2: ExpressionNode;
+  private _operand1: ExpressionNode;
+  private _operand2: ExpressionNode;
 
 
   constructor(operand1: ExpressionNode, operand2: ExpressionNode) {
     super();
-    this.operand1 = operand1;
-    this.operand2 = operand2;
+    this._operand1 = operand1;
+    this._operand2 = operand2;
   }
 
   protected abstract getOperatorString(): string;
+
+
+  public get operand1(): ExpressionNode {
+    return this._operand1;
+  }
+
+  public get operand2(): ExpressionNode {
+    return this._operand2;
+  }
+
 
   public toString(): string {
     return "(" + this.operand1.toString() + " " + this.getOperatorString() + " " + this.operand2.toString() + ")";
--- src/TreeNodes/UnaryOperator.ts
+++ src/TreeNodes/UnaryOperator.ts
@@ -1,16 +1,21 @@
 import { ExpressionNode } from "./ExpressionNode.ts"
 
 export abstract class UnaryOperator extends ExpressionNode {
-  protected operand: ExpressionNode;
+  protected _operand: ExpressionNode;
 
   constructor(operand: ExpressionNode) {
     super();
-    this.operand = operand;
+    this._operand = operand;
   }
 
   protected abstract getOperatorString(): string;
 
+
+  public get operand() : ExpressionNode {
+    return this._operand;
+  }
+
   public toString(): string {
-    return this.getOperatorString() + "(" + this.operand.toString() + ")";
+    return this.getOperatorString() + "(" + this._operand.toString() + ")";
   }
 }
