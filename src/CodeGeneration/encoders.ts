1b52f14069d09c123af5d5505f2dd38040e9fbb1
AndreJesusBrito <andrejesusdebrito@gmail.com>
2020-07-20 11:28:37 +0100

    moved encoder functions to separeted file

---
+++ src/CodeGeneration/encoders.ts
@@ -0,0 +1,94 @@
+export function encodeVector(elements: any[]): number[] {
+  // const buffer = new ArrayBuffer(4 + elements.length);
+
+  return [
+    ...encodeU32(elements.length),
+    ...elements.flat()
+  ];
+}
+
+// just a puts the bytes length in front
+export function encodeContainer(data: any[]): number[] {
+  const flatData = [...data.flat()];
+  return [
+    ...encodeU32(flatData.length),
+    ...flatData,
+  ];
+}
+
+
+export function encodeSection(sectionId: number, data: any[]): number[] {
+  return [
+    sectionId,
+    ...encodeContainer(data)
+  ]
+}
+
+export function encodeFuncType(paramsVec: any[], resultVec: any[]): number[] {
+  return [
+    0x60,
+    ...encodeVector(paramsVec),
+    ...encodeVector(resultVec),
+  ];
+}
+
+export function encodeI32(n: number) {
+  const result = signedLEB128(n);
+  console.assert(result.length <= Math.ceil(32 / 7));
+  return result;
+}
+export function encodeU32(n: number) {
+  const result = unsignedLEB128(n);
+  console.assert(result.length <= Math.ceil(32 / 7));
+  return result;
+}
+
+export function signedLEB128(n: number): number[] {
+  const buffer = [];
+  let done = false;
+
+  while (!done) {
+    let byte = n & 0x7f;
+    n >>>= 7;
+
+    done = (n === 0 && (byte & 0x40) === 0)
+      || (n === -1 && (byte & 0x40) !== 0);
+
+    if (!done) {
+      byte |= 0x80;
+    }
+
+    buffer.push(byte);
+  }
+
+  return buffer;
+};
+
+export function unsignedLEB128(n: number): number[] {
+  const buffer = [];
+
+  do {
+    let byte = n & 0x7f;
+    n >>>= 7;
+
+    if (n !== 0) {
+      byte |= 0x80;
+    }
+
+    buffer.push(byte);
+  } while (n !== 0);
+
+  return buffer;
+}
+
+// only ascii for now
+export function encodeName(str: string): number[] {
+  return [
+    ...encodeU32(str.length),
+
+    // not sure if works for cjk chars.
+    // the problem could be from the name for exports
+    // and not from encoding (TODO: check later)
+    ...((new TextEncoder()).encode(str))
+  ]
+}
--- src/CodeGeneration/genCode.ts
+++ src/CodeGeneration/genCode.ts
@@ -1,100 +1,24 @@
 import {SectionTypeCode, ValueType, Opcode, ExportTypeCode} from "./codes.ts";
+import {
+  encodeU32,
+  encodeI32,
+  encodeName,
+  encodeVector,
+  encodeContainer,
+  encodeSection,
+  encodeFuncType,
+} from "./encoders.ts";
 
 const magicModuleHeader = [0x00, 0x61, 0x73, 0x6d];
 const moduleVersion = [0x01, 0x00, 0x00, 0x00];
 
-function encodeVector(elements: any[]): number[] {
-  // const buffer = new ArrayBuffer(4 + elements.length);
-
-  return [
-    ...encodeU32(elements.length),
-    ...elements.flat()
-  ];
-}
-
-// just a puts the bytes length in front
-function encodeContainer(data: any[]): number[] {
-  const flatData = [...data.flat()];
-  return [
-    ...encodeU32(flatData.length),
-    ...flatData,
-  ];
-}
 
 
-function encodeSection(sectionId: number, data: any[]): number[] {
-  return [
-    sectionId,
-    ...encodeContainer(data)
-  ]
-}
 
-function encodeFuncType(paramsVec: any[], resultVec: any[]): number[] {
-  return [
-    0x60,
-    ...encodeVector(paramsVec),
-    ...encodeVector(resultVec),
-  ];
-}
 
-function encodeI32(n: number) {
-  const result = signedLEB128(n);
-  console.assert(result.length <= Math.ceil(32 / 7));
-  return result;
-}
-function encodeU32(n: number) {
-  const result = unsignedLEB128(n);
-  console.assert(result.length <= Math.ceil(32 / 7));
-  return result;
-}
-
-function signedLEB128(n: number): number[] {
-  const buffer = [];
-  let done = false;
-
-  while (!done) {
-    let byte = n & 0x7f;
-    n >>>= 7;
-
-    done = (n === 0  && (byte & 0x40) === 0)
-        || (n === -1 && (byte & 0x40) !== 0);
-
-    if (!done) {
-      byte |= 0x80;
-    }
-
-    buffer.push(byte);
   }
 
-  return buffer;
-};
 
-function unsignedLEB128(n: number): number[] {
-  const buffer = [];
-
-  do {
-    let byte = n & 0x7f;
-    n >>>= 7;
-
-    if (n !== 0) {
-      byte |= 0x80;
-    }
-
-    buffer.push(byte);
-  } while (n !== 0);
-
-  return buffer;
-}
-
-// only ascii for now
-function encodeName(str: string): number[] {
-  return [
-    ...encodeU32(str.length),
-
-    // not sure if works for cjk chars (TODO: check later)
-    ...((new TextEncoder()).encode(str))
-  ]
-}
 
 export function genCode(): Uint8Array {
   return Uint8Array.from([
