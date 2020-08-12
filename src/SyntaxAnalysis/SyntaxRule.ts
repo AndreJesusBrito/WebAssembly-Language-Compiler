ee4a2fd7127d4bb2435ac8c166f4a0e880c7857f
AndreJesusBrito <andrejesusdebrito@gmail.com>
2020-08-12 17:03:27 +0100

    checkpoint 3

--- src/SyntaxAnalysis/SyntaxRule.ts
+++ src/SyntaxAnalysis/SyntaxRule.ts
@@ -2,52 +2,23 @@
 
 export class SyntaxRule {
   private derivations: Map<string, any>;
-  private _name: string;
 
   constructor(name: string) {
-    this._name = name;
     this.derivations = new Map();
   }
 
 
-  public get name() : string {
-    return this._name;
-  }
-
   public getDerivation(terminal: string): any {
-    const derivation = this.derivations.get(terminal);
-    if (derivation) {
-      return {
-        derivationSymbols: [2].reverse(),
-        actions: derivation.actions,
-      };
-    } else {
+    // if (false) {
+      // return {
+      //   derivationSymbols: [2].reverse(),
+      //   actions: derivation.actions,
+      // };
+    // } else {
       throw SyntaxError("Unexpected token '" + terminal + "' at rule " + this);
-    }
-  }
-
-  public setDerivation(rule: any[], actions: any[], ...terminals: string[]): void {
-    for (const terminal of terminals) {
-      if (this.derivations.get(terminal) instanceof Array) {
-        console.warn("'" + terminal + "' repeated terminal for rule " + this._name);
-      }
-      this.derivations.set(terminal, {
-        derivationSymbols: rule,
-        actions: actions,
-      });
-    }
-  }
-
-  public toString(): string {
-    let str = "SyntaxRule " + this._name + " {  ";
-    for (const [terminal, rule] of this.derivations) {
-      str += "'" + terminal + "' => [";
-
-      str += rule.derivationSymbols.map(() => {
-        return (Math instanceof SyntaxRule) ? 2 : 2
-      }).join("  ") + "]     ";
-    }
-
-    return str + "  }";
+    // }
   }
 }
+
+const rule = new SyntaxRule("none");
+rule.getDerivation("");
